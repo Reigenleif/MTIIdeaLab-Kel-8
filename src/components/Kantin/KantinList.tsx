c2faa681e658b5bbfe80e3db5862b03dfa787795
16721237@mahasiswa.itb.ac.id <16721237@mahasiswa.itb.ac.id>
2022-08-19 22:05:16 +0700

    minor patches

--- src/components/Kantin/KantinList.tsx
+++ src/components/Kantin/KantinList.tsx
@@ -3,20 +3,25 @@ import { DUMMY_KANTIN, Kantin } from "../../storage/kantin";
 
 export default function KantinList() {
   const KantinItem = (p: Kantin) => {
+    const kantinNavigator = () => {
+      window.open(p.mapLink,'_blank')
+    }
+
     return (
       <Flex
         bg="tosca.a"
-        w="min(70%,30em)"
+        w="100%"
         h="4.5em"
         borderRadius="6px"
         m="auto"
         mt="1em"
         p="0.5em"
         color="tosca.b"
-        
+        fontSize={["0,7em","0,9em","1em"]}
+        onClick={kantinNavigator}
       >
-        <Box w="70%" h="100%" display="inline-block"><Text textAlign='left'>{p.name}</Text><Text textAlign='left' fontSize="0.8em">{p.location}</Text></Box>
-        <Box w="20%" h="100%" display="inline-block"><Text textAlign='right' fontSize="1.4em">10 m</Text></Box>
+        <Box w="70%" h="100%" display="inline-block"><Text textAlign='left' noOfLines={1}>{p.name}</Text><Text textAlign='left' fontSize="0.8em" noOfLines={2}>{p.location}</Text></Box>
+        <Box w="20%" h="100%" display="inline-block"><Text textAlign='right' fontSize="1.4em" >10 m</Text></Box>
       </Flex>
     );
   };
